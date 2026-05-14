86cdcb45dbc3c69cb85c5eed639d9e1647178b11
Ashishkumar-NIT <ashishkumar05164@gmail.com>
2026-05-14 15:21:34 +0530

    chore: authorize app subdomain

--- next.config.mjs
+++ next.config.mjs
@@ -28,7 +28,7 @@ const nextConfig = {
     ],
   },
   serverActions: {
-    allowedOrigins: ['jewelindia.shop', '*.jewelindia.shop', 'localhost:3000'],
+    allowedOrigins: ['app.jewelindia.shop', '*.jewelindia.shop', 'localhost:3000'],
   },
   async redirects() {
     return [
